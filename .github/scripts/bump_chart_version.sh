40f8d98c189663c334b8c3342722825a6ef8eb71
slydlake <sly.dlake@gmail.com>
2025-09-23 20:10:29 +0200

    added: dependabot configuration for wg-easy updates changed: bump_chart_version script to include values.yaml for image annotations

--- .github/scripts/bump_chart_version.sh
+++ .github/scripts/bump_chart_version.sh
@@ -7,6 +7,7 @@ CHART_DIR="$1"
 GITHUB_TOKEN="$2"
 REPO="slydlake/helm-charts"
 CHART_YAML="$CHART_DIR/Chart.yaml"
+VALUES_YAML="$CHART_DIR/values.yaml"
 
 # Get chart name and version
 CHART_NAME=$(yq '.name' "$CHART_YAML")
@@ -22,6 +23,19 @@ if [ -n "$TAG_EXISTS" ]; then
   NEW_VERSION=$(echo $CHART_VERSION | awk -F. '{OFS="."; $NF+=1; print $0}')
   yq -i ".version = \"$NEW_VERSION\"" "$CHART_YAML"
   echo "Bumped version to $NEW_VERSION"
+
+  # Update artifacthub.io/images annotation
+  IMAGE_REPO=$(yq '.image.repository' "$VALUES_YAML")
+  IMAGE_TAG=$(yq '.image.tag' "$VALUES_YAML")
+  IMAGE_DIGEST=$(yq '.image.digest' "$VALUES_YAML")
+  IMAGE_NAME=$CHART_NAME
+
+  # Construct the new images string
+  NEW_IMAGES="- name: $IMAGE_NAME
+  image: $IMAGE_REPO:$IMAGE_TAG@$IMAGE_DIGEST"
+
+  yq -i ".annotations.\"artifacthub.io/images\" = \"$NEW_IMAGES\"" "$CHART_YAML"
+  echo "Updated artifacthub.io/images annotation"
 else
   echo "Tag $TAG does not exist. No bump needed."
 fi
